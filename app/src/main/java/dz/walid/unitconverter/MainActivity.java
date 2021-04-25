9b2c8186ec739cbb6fd5a61b76d4cb5df3e81dce
HP <walid.mokhtari@etudiant.univ-perp.fr>
2021-04-25 18:02:07 +0200

    Mise a jour 25/04/21 18:00

--- app/src/main/java/dz/walid/unitconverter/MainActivity.java
+++ app/src/main/java/dz/walid/unitconverter/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,10 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference().child("1");
+        myRef.setValue("Hellooo");
 
-        myRef.setValue("Hellooo 1");
+        HashMap<String,String> distanceMap = new HashMap<>();
+
+
+        distanceMap.put("mm","200");
+        distanceMap.put("cm","20");
+        distanceMap.put("dcm","2");
+
+        myRef.setValue(distanceMap);
+
+
 
     }
 }

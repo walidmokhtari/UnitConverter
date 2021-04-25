a9fb0f8b175652dbf97211b1cd4d6db39bc1d15c
HP <walid.mokhtari@etudiant.univ-perp.fr>
2021-04-26 01:05:42 +0200

    Mise a jour 26/04/21 01:05

---
+++ app/src/main/java/dz/walid/unitconverter/Controllers/LiquidActivity.java
@@ -0,0 +1,16 @@
+package dz.walid.unitconverter.Controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dz.walid.unitconverter.R;
+
+public class LiquidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liquid);
+    }
+}
--- app/src/main/java/dz/walid/unitconverter/Controllers/ServiceActivity.java
+++ app/src/main/java/dz/walid/unitconverter/Controllers/ServiceActivity.java
@@ -17,11 +17,38 @@ public class ServiceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_service);
 
         final Button b_d = findViewById(R.id.b_d);
+        final Button b_w = findViewById(R.id.b_w);
+        final Button b_t = findViewById(R.id.b_t);
+        final Button b_lp = findViewById(R.id.b_lp);
 
         b_d.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ServiceActivity.this, DistanceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        b_w.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ServiceActivity.this, WeightActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        b_t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ServiceActivity.this, TimeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        b_lp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ServiceActivity.this, LiquidActivity.class);
                 startActivity(intent);
             }
         });
